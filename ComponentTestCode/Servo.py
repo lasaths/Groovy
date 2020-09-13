9f98d7bea81c04678e2dc9bea750760e15066c06
lasaths <lasaths@hotmail.com>
2020-09-13 16:02:17 +0200

    Servo Change

---
+++ ComponentTestCode/Servo.py
@@ -0,0 +1,16 @@
+import RPi.GPIO as GPIO
+import time
+SERV = 6
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(SERV, GPIO.OUT)
+p = GPIO.PWM(SERV, 50)
+p.start(7.5)
+while True:
+        x = input();
+        if x=='x':
+            GPIO.output(SLP, GPIO.LOW) #No Cleanup GPIO as it makes Stepper go undefined and start turning
+                break
+        else:
+                p.ChangeDutyCycle(int(x))
+                print("Speed is " + x)
+                time.sleep(2)
