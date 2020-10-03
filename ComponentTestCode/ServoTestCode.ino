159f39494c46fc9be7c3b5519b3d3f39daaabcbd
lasaths <lasaths@hotmail.com>
2020-10-03 13:20:01 +0200

    Update ServoTestCode.ino

--- ComponentTestCode/ServoTestCode.ino
+++ ComponentTestCode/ServoTestCode.ino
@@ -5,7 +5,7 @@ Servo myservo;
 void setup() 
 { 
   myservo.attach(9);
-  myservo.write(150);  // set servo to mid-point
+  myservo.write(150);  // set servo to mid-point (150 Fast)
 } 
 
 void loop() {} 
